e1938f1a26876a3b109ffda5d10bc4a99be7bc8d
Corey Solitaire <corey.solitaire@gmail.com>
2020-09-24 12:26:36 -0500

    Imports Correctly

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-import os
-from env import host, user, password
+import scipy as sp 
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 ###################### Acquire Telco Churn Data ######################
 
@@ -10,44 +14,20 @@ def train_valid_test(df):
     train, validate = train_test_split(train_validate, test_size = .3, random_state = 123, stratify = train_validate.churn)
     return train, validate, test
 
-def get_connection(db, user=user, host=host, password=password):
+def wrangle_telco(df):
+    ''' 
+    This function:
+    1. Reads in the telco data from a csv file
+    2. Changes total_charges to a numeric variable and replaces any NaN values with a 0
+    3. Splits data in to train, validate, test and returns as dataframes     
     '''
-    This function uses my info from my env file to
-    create a connection url to access the Codeup db.
-    '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-
-def new_telco_data():
-    '''
-    This function reads the telco data from the Codeup db into a df,
-    writes it to a csv file, and returns the df.
-    '''
-    sql_query = '''
-                Select
-                customer_id,
-                monthly_charges,
-                tenure,
-                total_charges
-                From customers
-                Where contract_type_id = 3;
-                '''
-    df = pd.read_sql(sql_query, get_connection('telco_churn'))
     # Changes total_charges to numeric variable
     df['total_charges'] = pd.to_numeric(df['total_charges'],errors='coerce')
     # Replaces NaN values with 0 for new customers with no total_charges
     df["total_charges"].fillna(0, inplace = True) 
-    df.to_csv('telco_churn.csv')
-    train, validate, test = train_valid_test(df)
-    return train, validate, test
-
-def wrangle_telco(cached=False):
-    '''
-    This function reads in the telco data from Codeup database if cached == False
-    or if cached == True reads in telco df from a csv file, returns df
-    '''
-    if cached or os.path.isfile('telco_churn.csv') == False:
-        df = new_telco_data()
-    else:
-        df = pd.read_csv('telco_churn.csv', index_col=0)
     return df
+    #train, validate, test = train_valid_test(df)
+    #return train, validate, test
+
+
 
