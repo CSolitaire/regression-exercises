d5ea66d121e6df8072106aae89deaebf69935926
Corey Solitaire <corey.solitaire@gmail.com>
2020-09-26 15:58:33 -0500

    removed stratify from function

--- split_scale.py
+++ split_scale.py
@@ -10,8 +10,12 @@ from sklearn.preprocessing import StandardScaler
 ###################### Split Telco Churn Data ######################
 
 def train_valid_test(df):
-    train_validate, test = train_test_split(df, test_size = .2, random_state = 123, stratify = df.total_charges)
-    train, validate = train_test_split(train_validate, test_size = .3, random_state = 123, stratify = train_validate.total_charges)
+    '''
+    This function takes in a prepared dataframe and splits that data in to train, valid, split
+    ** NOTE:**  No stratify in regression, only in used in classificaiton on labeled data
+    '''
+    train_validate, test = train_test_split(df, test_size = .2, random_state = 123)
+    train, validate = train_test_split(train_validate, test_size = .3, random_state = 123)
     return train, validate, test
 
 ###################### Scale Telco Churn Data ######################
