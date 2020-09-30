26a656ddb189a6cd1cc892828b7cb0d4a7ea123b
Corey Solitaire <corey.solitaire@gmail.com>
2020-09-30 10:20:07 -0500

    small edits after walkthrough

--- explore.py
+++ explore.py
@@ -17,43 +17,22 @@ def plot_variable_pairs(df):
     g.map_diag(sns.distplot)  
     g.map_offdiag(sns.regplot)
     plt.show()
-
-
-def plot_variable_pairs_alt(df):
-    '''
-    Function that plots all of the pairwise relationships along with the regression line for each pair
-    '''
-    y = df.iloc[:,3]
-    X = df.iloc[:,1]
-    data = df
-    return sns.regplot(X, y, ci = None)
+    return g
 
 
 def months_to_years(df):
     '''
     Function that returns your dataframe with a new feature tenure_years, in complete years as a customer
     '''
-    df['tenure_years'] = round(df.tenure / 12, 0)
-    return df
+    return df.assign(tenure_years=(df.tenure / 12).apply(math.floor))
 
 
-def months_to_years_alt(tenure_months, df):
-    '''
-    Function that returns your dataframe with a new feature tenure_years, in complete years as a customer
-    '''
-    df['tenure_years'] = round(tenure_months / 12, 0)
-    return df
-
-
-def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
+def plot_categorical_and_continuous_vars(df, categorical_var, continuous_var):
     '''
     Function that outputs 3 different plots for plotting a categorical variable with a continuous variable
     '''
-    plt.rc('font', size=13)
-    plt.rc('figure', figsize=(13, 7))
-    sns.boxplot(data = df, y=continuous_var, x=categorical_var)
+    sns.barplot(data=df, y=continuous_var, x=categorical_var)
     plt.show()
-    sns.violinplot(data = df, y=continuous_var, x=categorical_var)
+    sns.violinplot(data=df, y=continuous_var, x=categorical_var)
     plt.show()
-    sns.swarmplot(data = df, y=continuous_var, x=categorical_var)
-    plt.show()
+    sns.boxplot(data=df, y=continuous_var, x=categorical_var)
